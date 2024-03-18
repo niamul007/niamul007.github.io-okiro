076b56249e1ad0a127a85bd25f2286b6b2632993
niamul007 <am3994566@gmail.com>
2024-03-18 23:33:10 +0600

    add a new div

--- index.js
+++ index.js
@@ -3,9 +3,28 @@ function show(){
     let icon = document.querySelector(".icon-nav");
     icon.classList.toggle('nva-icon');
 }
+// function showMenu() {
+//     let menu = document.querySelector(".side-menu");
+//     if (menu.style.display === "flex") {
+//         menu.style.display = "none";
+//     } else {
+//         menu.style.display = "flex";
+//     }
+// }
 
+function toggleMenu() {
+    var menu = document.getElementById('menu');
+    var bar = document.getElementById('bar');
+    var close = document.getElementById('close');
 
-function showMenu(){
-    let menu = document.querySelector(".side-menu");
-    menu.style.display="flex";
-}
+    // Toggle visibility of the menu
+    if (menu.classList.contains('visible')) {
+      menu.classList.remove('visible');
+      bar.style.display = 'block'; // Show the bar symbol
+      close.style.display = 'none'; // Hide the close symbol
+    } else {
+      menu.classList.add('visible');
+      bar.style.display = 'none'; // Hide the bar symbol
+      close.style.display = 'block'; // Show the close symbol
+    }
+  }
