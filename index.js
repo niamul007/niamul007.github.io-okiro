4f34c30a0a26875ae404d8816a3c94f7854548c1
niamul007 <am3994566@gmail.com>
2024-03-18 15:50:41 +0600

    work in on navbar

---
+++ index.js
@@ -0,0 +1,11 @@
+
+function show(){
+    let icon = document.querySelector(".icon-nav");
+    icon.classList.toggle('nva-icon');
+}
+
+
+function showMenu(){
+    let menu = document.querySelector(".side-menu");
+    menu.style.display="flex";
+}
