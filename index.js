a9c97707abccc83ed9f448afa9231faf814a6964
niamul007 <am3994566@gmail.com>
2024-03-20 19:13:02 +0600

    addd minor changes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-
-function show(){
-    let icon = document.querySelector(".icon-nav");
-    icon.classList.toggle('nva-icon');
+function show() {
+  let icon = document.querySelector(".icon-nav");
+  icon.classList.toggle("nva-icon");
 }
 // function showMenu() {
 //     let menu = document.querySelector(".side-menu");
@@ -13,18 +12,27 @@ function show(){
 // }
 
 function toggleMenu() {
-    var menu = document.getElementById('menu');
-    var bar = document.getElementById('bar');
-    var close = document.getElementById('close');
+  var menu = document.getElementById("menu");
+  var bar = document.getElementById("bar");
+  var close = document.getElementById("close");
 
-    // Toggle visibility of the menu
-    if (menu.classList.contains('visible')) {
-      menu.classList.remove('visible');
-      bar.style.display = 'block'; // Show the bar symbol
-      close.style.display = 'none'; // Hide the close symbol
-    } else {
-      menu.classList.add('visible');
-      bar.style.display = 'none'; // Hide the bar symbol
-      close.style.display = 'block'; // Show the close symbol
-    }
+  // Toggle visibility of the menu
+  if (menu.classList.contains("visible")) {
+    menu.classList.remove("visible");
+    bar.style.display = "block"; // Show the bar symbol
+    close.style.display = "none"; // Hide the close symbol
+  } else {
+    menu.classList.add("visible");
+    bar.style.display = "none"; // Hide the bar symbol
+    close.style.display = "block"; // Show the close symbol
   }
+}
+function large() {
+  let emailDiv = document.querySelector(".input-btn");
+  emailDiv.classList.toggle("in-btn-click");
+}
+
+function small() {
+  let emailDiv = document.querySelector(".input-btn");
+  emailDiv.classList.add("in-btn-click-out");
+}
